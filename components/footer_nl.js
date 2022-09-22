52cd5f0db6c8e600eee66197f69ba8665b474895
Michal0616 <111012759+Michal0616@users.noreply.github.com>
2022-09-22 03:15:00 -0700

    Update footer_nl.js

--- components/footer_nl.js
+++ components/footer_nl.js
@@ -180,6 +180,7 @@ export default function Footer() {
           </div>
         </div> */}
         <div className="footer-middle row p-0 m-0 pt-5">
+          <div>Hello wadfasdf</div>
           {midFooterList.map((items, index) => {
             return (
               <div
